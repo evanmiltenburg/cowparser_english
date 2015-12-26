95b43566909a107e3ed22f6df35fec8ea21ee4d5
Emiel van Miltenburg <emielonline@gmail.com>
2015-12-26 08:54:12 +0100

    check tag in parser rather than in the loop

--- encowparser.py
+++ encowparser.py
@@ -58,27 +58,25 @@ def sentence_generator(filename, gzipped=True, structure=False):
     # Memory usage lowered using:
     # http://stackoverflow.com/a/12161078/2899924
     source = gzip.GzipFile(filename) if gzipped else filename
-    parser = etree.iterparse(source, html=True, events=('start',))
+    parser = etree.iterparse(source, html=True, events=('start',), tag='s')
     if structure:
         # element.attrib() returns a dictionary with metadata for the sentence.
         # get_full_sentence_data() returns the structure and a list of tokens
         for event, element in parser:
-            if element.tag == 's':
-                yield (element.attrib, get_full_sentence_data(element))
-                element.clear()
-                for ancestor in element.xpath('ancestor-or-self::*'):
-                    while ancestor.getprevious() is not None:
-                        del ancestor.getparent()[0]
+            yield (element.attrib, get_full_sentence_data(element))
+            element.clear()
+            for ancestor in element.xpath('ancestor-or-self::*'):
+                while ancestor.getprevious() is not None:
+                    del ancestor.getparent()[0]
     else:
         # element.attrib() returns a dictionary with metadata for the sentence.
         # get_sentence_data() returns a list of tokens
         for event, element in parser:
-            if element.tag == 's':
-                yield (element.attrib, get_sentence_data(element))
-                element.clear()
-                for ancestor in element.xpath('ancestor-or-self::*'):
-                    while ancestor.getprevious() is not None:
-                        del ancestor.getparent()[0]
+            yield (element.attrib, get_sentence_data(element))
+            element.clear()
+            for ancestor in element.xpath('ancestor-or-self::*'):
+                while ancestor.getprevious() is not None:
+                    del ancestor.getparent()[0]
     # Aggressively keep memory load down
     del parser
 
